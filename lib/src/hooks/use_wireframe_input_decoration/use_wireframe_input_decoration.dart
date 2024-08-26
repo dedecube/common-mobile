7f5f02ce576b0f03f39b8d09013333711937bb92
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-08-26 20:56:10 +0200

    fix: remove useMemoized from useWireframe* hooks

--- lib/src/hooks/use_wireframe_input_decoration/use_wireframe_input_decoration.dart
+++ lib/src/hooks/use_wireframe_input_decoration/use_wireframe_input_decoration.dart
@@ -10,36 +10,34 @@ InputDecoration useWireframeInputDecoration(
   final hintTranslationKey = '$translationKey.hint';
   final labelTranslationKey = '$translationKey.label';
 
-  return useMemoized(
-    () => InputDecoration(
-      errorText: error,
-      hintText: context.translate(hintTranslationKey),
-      alignLabelWithHint: true,
-      hintStyle: TextStyle(
-        fontWeight: FontWeight.w400,
+  return InputDecoration(
+    errorText: error,
+    hintText: context.translate(hintTranslationKey),
+    alignLabelWithHint: true,
+    hintStyle: TextStyle(
+      fontWeight: FontWeight.w400,
+      color: context.theme.surfaceInverse100,
+      fontSize: 16,
+    ),
+    labelText: context.translate(labelTranslationKey),
+    labelStyle: TextStyle(
+      fontWeight: FontWeight.w400,
+      color: context.theme.surfaceInverse100,
+      fontSize: 16,
+    ),
+    border: OutlineInputBorder(
+      borderSide: BorderSide(
         color: context.theme.surfaceInverse100,
-        fontSize: 16,
       ),
-      labelText: context.translate(labelTranslationKey),
-      labelStyle: TextStyle(
-        fontWeight: FontWeight.w400,
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(
         color: context.theme.surfaceInverse100,
-        fontSize: 16,
       ),
-      border: OutlineInputBorder(
-        borderSide: BorderSide(
-          color: context.theme.surfaceInverse100,
-        ),
-      ),
-      enabledBorder: OutlineInputBorder(
-        borderSide: BorderSide(
-          color: context.theme.surfaceInverse100,
-        ),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderSide: BorderSide(
-          color: context.theme.surfaceInverse100,
-        ),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: context.theme.surfaceInverse100,
       ),
     ),
   );
--- lib/src/hooks/use_wireframe_text_field_text_style/use_wireframe_text_field_text_style.dart
+++ lib/src/hooks/use_wireframe_text_field_text_style/use_wireframe_text_field_text_style.dart
@@ -4,11 +4,9 @@ import 'package:velvet_framework/velvet_framework.dart';
 TextStyle useWireframeTextFieldTextStyle() {
   final context = useContext();
 
-  return useMemoized(
-    () => TextStyle(
-      fontWeight: FontWeight.w400,
-      color: context.theme.surfaceInverse100,
-      fontSize: 16,
-    ),
+  return TextStyle(
+    fontWeight: FontWeight.w400,
+    color: context.theme.surfaceInverse100,
+    fontSize: 16,
   );
 }
