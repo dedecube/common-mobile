e6dcf71df2baefbc6e9e170dc51f4f8f837a58ab
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-08-26 15:29:22 +0200

    chore: update exports

--- lib/common_mobile.dart
+++ lib/common_mobile.dart
@@ -1,8 +1,14 @@
 library common_mobile;
 
 // @index(['./**/(*.dart)','!./**/*.g.dart', '!./**/_*.dart'], f => `export '${f.path}.dart';`)
+export './src/components/layout/dede_align.dart';
+export './src/components/layout/dede_padding.dart';
+export './src/components/layout/dede_safe_space.dart';
+export './src/components/layout/dede_safe.dart';
+export './src/components/layout/dede_space.dart';
 export './src/extensions/build_context/shortcuts_on_build_context_extension.dart';
 export './src/extensions/build_context/unfocus_on_build_context_extension.dart';
+export './src/extensions/list/separate_on_list_extension.dart';
 export './src/extensions/num/int/duration_on_int_extension.dart';
 export './src/extensions/num/int/inverted_index_on_int_extension.dart';
 export './src/extensions/num/modulo_less_than_on_num_extension.dart';
